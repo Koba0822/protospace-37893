34a86735a5c4cd017b45bacfc632635e74dc172c
Koba0822 <o93149314@icloud.com>
2022-09-04 06:58:00 +0900

    ここまで正常

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,19 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+end
